a4e58cd477343cf01c3bfd50f0111de53a9a5a06
“Tanvika <3037916s@student.gla.ac.uk>
2025-02-11 01:12:12 +0000

    second commit

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,9 @@ def index(request):
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <br><a href='/rango/'>Back to Index</a>")
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 
