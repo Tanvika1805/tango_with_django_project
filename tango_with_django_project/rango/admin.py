651c72a9f6ffd78347db6136da0c682fb2223e34
“Tanvika <3037916s@student.gla.ac.uk>
2025-02-11 23:06:42 +0000

    third commit

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')   # Display these fields in the admin panel
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')  # Optional: Display extra fields
+
+# Register models with their custom admin views
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
 
 # Register your models here.
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,10 +5,12 @@ def index(request):
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <br><a href='/rango/'>Back to Index</a>")
+    
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
-
+def about(request):
+    return render(request, 'rango/about.html')
 
 # Create your views here.
